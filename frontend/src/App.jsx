b973c8439b8cf7b24dc54a7158118e84aae19e22
sreejareddy03 <sreejaa3115@gmail.com>
2026-07-04 11:34:29 +0530

    Added footer component and completed frontend navigation layout

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,11 +1,15 @@
 import Navbar from "./components/Navbar";
+import Footer from "./components/Footer";
 import AppRoutes from "./routes/AppRoutes";
 
 function App() {
   return (
     <>
       <Navbar />
+
       <AppRoutes />
+
+      <Footer />
     </>
   );
 }
---
+++ frontend/src/components/Footer.jsx
@@ -0,0 +1,19 @@
+import "./Footer.css";
+
+function Footer() {
+  return (
+    <footer className="footer">
+      <p>© 2026 Medical Research Paper Assistant </p>
+
+      <div className="footer-links">
+        <span>Home</span>
+        <span>Upload</span>
+        <span>Search</span>
+        <span>Chat</span>
+        <span>About</span>
+      </div>
+    </footer>
+  );
+}
+
+export default Footer;
