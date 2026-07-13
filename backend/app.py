3df5dcb6be7485baa3661f3a3017cb492b7adb3a
AkulKalyan <akulkalyan@gmail.com>
2026-07-13 10:16:36 +0530

    “updated”

--- backend/app.py
+++ backend/app.py
@@ -1,29 +1,32 @@
-from fastapi import FastAPI
-from fastapi.middleware.cors import CORSMiddleware
+# from fastapi import FastAPI
+# from fastapi.middleware.cors import CORSMiddleware
 
-from api import router
+# from api import router
+# #-------------------------------------------------- --- IGNORE ---
+# app = FastAPI(
+#     title="MRP Medical Research RAG API"
+# )
 
-app = FastAPI(
-    title="MRP Medical Research RAG API"
-)
+# app.add_middleware(
+#     CORSMiddleware,
+#     allow_origins=[
+#         "http://localhost:5173",
+#         "http://127.0.0.1:5173",
+#         "https://medical-research-paper-ai.up.railway.app",
+#     ],
+#     allow_credentials=False, # Temp dev
+#     allow_methods=["*"],
+#     allow_headers=["*"],
+# )
 
-app.add_middleware(
-    CORSMiddleware,
-    allow_origins=[
-        "http://localhost:5173",
-        "http://127.0.0.1:5173",
-        "https://medical-research-paper-ai.up.railway.app",
-    ],
-    allow_credentials=False, # Temp dev
-    allow_methods=["*"],
-    allow_headers=["*"],
-)
+# app.include_router(router, prefix="/api")
 
-app.include_router(router, prefix="/api")
+# #-------------------------------------------------- --- IGNORE ---
+# # Root Endpoint --- IGNORE ---
 
 
-@app.get("/")
-def root():
-    return {
-        "message": "MRP RAG API is running"
-    }
+# @app.get("/")
+# def root():
+#     return {
+#         "message": "MRP RAG API is running"
+#     }
