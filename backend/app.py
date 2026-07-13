66faf07261252e4ebc7ac1feb90d4bf8df101d00
AkulKalyan <akulkalyan@gmail.com>
2026-07-13 10:18:11 +0530

    “updated”

--- backend/app.py
+++ backend/app.py
@@ -2,7 +2,6 @@ from fastapi import FastAPI
 from fastapi.middleware.cors import CORSMiddleware
 
 from api import router
-#-------------------------------------------------- --- IGNORE ---
 app = FastAPI(
     title="MRP Medical Research RAG API"
 )
@@ -14,15 +13,12 @@ app.add_middleware(
         "http://127.0.0.1:5173",
         "https://medical-research-paper-ai.up.railway.app",
     ],
-    allow_credentials=False, # Temp dev
+    allow_credentials=False,
     allow_methods=["*"],
     allow_headers=["*"],
 )
 
 app.include_router(router, prefix="/api")
-
-#-------------------------------------------------- --- IGNORE ---
-# Root Endpoint --- IGNORE ---
 
 
 @app.get("/")
